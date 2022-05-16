ec4e9b3ffd5537877f6fd7adefb924fd3452a068
Tuaan <vudinhtuan2000@gmail.com>
2022-05-16 23:00:16 +0700

    update

--- client/src/pages/admin/Order.js
+++ client/src/pages/admin/Order.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+// import { useState, useEffect } from 'react'
 import { TitleContent, TableContainer, ButtonCreate } from '../../components'
 import useTitle from '../../hooks/useTitle'
 
